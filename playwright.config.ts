6686a0afc7c7a4392728359862c31bd5fbc7a276
NikKalugin <expeditiontomysoul@gmail.com>
2026-09-15 08:43:26 +0300

    Passing env variables Topic

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
-
-declare const process: {
-    env: Record<string, string | undefined>;
-};
+import dotenv from 'dotenv';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+dotenv.config();
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -30,11 +27,11 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
-        baseURL: 'https://qauto.forstudy.space/',
+        baseURL: process.env.BASE_URL,
 
         httpCredentials: {
-            username: 'guest',
-            password: 'welcome2qauto'
+            username: process.env.HTTP_USERNAME!,
+            password: process.env.HTTP_PASSWORD!
         },
         testIdAttribute: 'qa-id',
 
